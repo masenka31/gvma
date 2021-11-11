3090bc7374ae1f9bdd14e22979fda16f0e78bd29
maskomic <maskomic@fjfi.cvut.cz>
2021-11-11 15:21:51 +0100

    results update

--- scripts/strain/reg_results.jl
+++ scripts/strain/reg_results.jl
@@ -4,15 +4,16 @@ using DrWatson
 using gvma
 using Flux, DataFrames, BSON
 using PrettyTables
+using Statistics
 
 # load data
 df = collect_results(datadir("regularization"))
-seeds = parse.(Int64, map(i -> match(r".*seed=([0-9]{1,2}).*", df[i,:path]).captures[1], 1:size(df, 1)))
-df[!,:seed] = seeds     # add seed
-sort!(df, [:seed, :reg])
+# when loaded from regularization_first, add seed
+# seeds = parse.(Int64, map(i -> match(r".*seed=([0-9]{1,2}).*", df[i,:path]).captures[1], 1:size(df, 1)))
+# df[!,:seed] = seeds     # add seed
 
 # group and combine
-g = groupby(df, :reg)
+g = groupby(df, [:α, :ratio])
 cdf = combine(
     g,
     :train_acc => mean,
@@ -29,4 +30,5 @@ cdf = combine(
 cdf_rounded = round.(cdf, digits=4)
 
 # print markdown table
-pretty_table(cdf_rounded, tf = tf_markdown, nosubheader=true)
+pretty_table(cdf_rounded[:, [2,1,3,4,5,6,7,8,9,10,11]],
+                tf = tf_markdown, nosubheader=true, body_hlines=collect(7:7:50))
